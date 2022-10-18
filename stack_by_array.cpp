192987cd231e879ff6848cab64b0ccac9ab86087
Vishal Gupta <114002191+vishalxgithub@users.noreply.github.com>
2022-10-18 21:17:53 +0530

    Create stack_by_array.cpp

---
+++ stack_by_array.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Stack{
+    int *arr = new int[100];
+    int top = -1;
+};
+void Push(Stack &s, int data){
+    s.top++;
+    s.arr[s.top] = data;
+}
+void Pop(Stack &s){
+    s.top--;
+}
+int Top(Stack s){
+    return s.arr[s.top];
+}
+int Size(Stack s){
+    return s.top+1;
+}
+bool isEmpty(Stack s){
+    return s.top == -1;
+}
+void printStack(Stack s){
+    for (int i = 0; i < s.top+1; i++)
+        cout<<s.arr[i]<<" ";
+    cout<<endl;
+}
+int main()
+{
+    Stack s;
+    Push(s,11);Push(s,12);Push(s,13);
+    printStack(s);
+    Pop(s);
+    printStack(s);
+    cout<<"Top value of the stack: "<<Top(s)<<endl;
+    cout<<"Size of the stack: "<<Size(s)<<endl;
+    cout<<"Is stack is empty: "<<isEmpty(s)<<endl;
+    return 0;
+}
