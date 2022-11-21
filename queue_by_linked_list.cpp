6467dea437db323cc462fada76db35bd69d0ae62
Vishal Gupta <114002191+vishalxgithub@users.noreply.github.com>
2022-11-21 21:29:10 +0530

    Update queue_by_linked_list.cpp

--- queue_by_linked_list.cpp
+++ queue_by_linked_list.cpp
@@ -31,25 +31,39 @@ class Queue{
 	//push the value at the back
 	void push(int val){
 		cout<<"pushing the val: "<<val<<" at the back of queue\n";
+		Node* newnode = createNode(val);
+		if(head == NULL)
+			head = tail = newnode;
+		else{
+				tail->next = newnode;
+				tail = newnode;
+		}
 	}
 	
 	int front(){
 		cout<<"returning the value at front\n";
-		return 0;
+		return tail->data;
 	}
 	
 	void pop(){
 		cout<<"removing the first element\n";
+		head = head->next;
 	}
 	
 	int size(){
 		cout<<"return the size of the queue\n";
-		return 0;
+		Node* temp = head;
+		int count = 0;
+		while(temp != tail){
+			count++;
+			temp = temp->next;
+		}
+		return count;
 	}
 	
 	bool isEmpty(){
 		cout<<"returning if the queue is empty\n";
-		return true;
+		return (head == NULL);
 	}
 };
 
